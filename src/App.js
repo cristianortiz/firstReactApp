c216751d28dedea5c9f4e47e096c9df284b3d60a
cristian <cristian.ortiz@uda.cl>
2020-05-01 15:14:17 -0400

    agregamos props al componente Helloworld y aprendemos como funcionan estos props

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import "./App.css";
 /*modelo de componentes de React, una aplicacion esta subdivida en componentes reutilzables, podemos escribir
 un componente de diversas formas, como funcion explicita, funcion flecha, clase JS */
 //funcion explicita
-function HelloWorld() {
-  return <div id="hello">Soy el componente 'Hello world'</div>;
+function HelloWorld(props) {
+  //props, un objeto con datos, para personalizar el renderizado de un mismo componente
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      Soy el componente 'Hello world' {props.mytext}
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      Componente APP llama componente HelloWorld:
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
+      <h2> Componente APP llama a 3 componentes "HelloWorld": </h2>
+      <HelloWorld
+        mytext=" 1 y tengo el prop 'mytext'"
+        subtitle="soy el prop 'subtitle' y estoy dentro de un h3"
+      />
+      <HelloWorld
+        mytext=" 2 con otro valor para el prop 'mytext'"
+        subtitle="soy el prop 'subtitle' del 2do comp. 'Helloworld'"
+      />
+      <HelloWorld
+        mytext=" 3 ultimo valor para prop 'mytext'"
+        subtitle="soy el prop 'subtitle' del 3er comp. 'Helloworld'"
+      />
     </div>
   );
 }
