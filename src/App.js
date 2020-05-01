2b07e86034ae3f05048f868b64281706c7481789
cristian <cristian.ortiz@uda.cl>
2020-05-01 14:50:35 -0400

    definicion de 2 componentes en App.js para explicar de forma simple el modelo de componentes de react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div>hello world</div>;
+/*modelo de componentes de React, una aplicacion esta subdivida en componentes reutilzables, podemos escribir
+un componente de diversas formas, como funcion explicita, funcion flecha, clase JS */
+//funcion explicita
+function HelloWorld() {
+  return <div id="hello">Soy el componente 'Hello world'</div>;
 }
 
+function App() {
+  return (
+    <div>
+      Componente APP llama componente HelloWorld:
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+    </div>
+  );
+}
 export default App;
