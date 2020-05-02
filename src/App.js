ac07c0d8a91dc74c3e7bf4b14f49723225953f36
cristian <cristian.ortiz@uda.cl>
2020-05-01 20:37:58 -0400

    Update App.js
    ejemplo de manejo de estados de un componente react mediante metodos de un componente escrito como clase y eventos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 /*modelo de componentes de React, una aplicacion esta subdivida en componentes reutilzables, podemos escribir
 un componente de diversas formas, como funcion explicita, funcion flecha, clase JS */
 //funcion explicita
-function HelloWorld(props) {
+/* function HelloWorld(props) {
   //props, un objeto con datos, para personalizar el renderizado de un mismo componente
   return (
     <div id="hello">
@@ -12,6 +12,36 @@ function HelloWorld(props) {
       Soy el componente 'Hello world' {props.mytext}
     </div>
   );
+} */
+//componente en forma de clase, cambia la forma de usar los props, con this, por el ambito de la clase
+class HelloWorld extends React.Component {
+  //manejo estados del componente con este objeto state
+  state = {
+    show: true,
+  };
+  //metodo para cambiar el estado de show a traves del evento onClick,notacion flecha evita escribir el bind en Onclick
+  togleShow = () => {
+    //cambia alternadamente a true/false el valor de show al presionar los botones
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          Soy el componente 'Hello world' {this.props.mytext}
+          <button onClick={this.togleShow}>Toggle state.show a false</button>
+        </div>
+      );
+    } else {
+      return (
+        <h3>
+          'state.show:false' =>No hay elementos que mostrar
+          <button onClick={this.togleShow}>Toggle state.show a true</button>
+        </h3>
+      );
+    }
+  }
 }
 
 function App() {
