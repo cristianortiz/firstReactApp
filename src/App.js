439c1d02aaf6bacd411d82124e4bc7a274f55280
cristian <cristian.ortiz@uda.cl>
2020-05-03 16:11:50 -0400

    creamos un metodo addTask para agregar una tarea al state tasks del componente App, que lo pasamos por props a otros componentes que lo pueden usar

--- src/App.js
+++ src/App.js
@@ -8,11 +8,25 @@ class App extends Component {
   state = {
     tasks: tasks_data, //le paso los datos de las tareas desde task.json
   };
+
+  //metodo para crear una nueva tarea, se puede usar a traves de propos en cualquier componente
+  addTask = (title, description) => {
+    //console.log("agregando un nueva tarea...");
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length,
+    };
+    this.setState({
+      //al arreglo de estados 'tasks' le agrego una nueva tarea
+      tasks: [...this.state.tasks, newTask],
+    });
+  };
   render() {
     //llamo al comp. TASKS que listara todas las tareas disponibles, le paso los datos de tasks
     return (
       <div>
-        <TaskForm />
+        <TaskForm addTask={this.addTask} />
         <Tasks tasks={this.state.tasks} />
       </div>
     );
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,8 +1,9 @@
-/*componente que crea un formulario para ingresar nuevas tareas*/
+/*componente que crea un formulario para ingresar nuevas tareas uso los eventos del form y de sus inputs para controlar
+de forma dinamica el estado de cada uno*/
 import React, { Component } from "react";
 
 export default class TaskForm extends Component {
-  //estados controlar dinamicamente el contenido de los inputs del formulario
+  //objeto que guarda el contenido de los inputs del formulario cuando se teclea en ellos
   state = {
     title: "",
     description: "",
@@ -12,11 +13,13 @@ export default class TaskForm extends Component {
   onSubmit = (event) => {
     console.log(this.state); //verifico el estado del los input del formulario al presionar 'Enviar'
     event.preventDefault(); //evito que se recargue la pagina al presionar 'Enviar'
+    //uso el mtodo addTask() del componente App a traves de props
+    this.props.addTask(this.state.title, this.state.description);
   };
   //escucha el evento onChange de cualquier input del formulario
   onChange = (event) => {
-    /*imprimo en consola la ejecucion del evento onChange
-    //console.log(event.target.value); */
+    //imprimo en consola la ejecucion del evento onChange
+    // console.log(event.target.value);
     /* como ambos inputs usan onChange  cada input se identifica con 'name' asi no interfieren en sus estados */
     this.setState({
       [event.target.name]: event.target.value,
