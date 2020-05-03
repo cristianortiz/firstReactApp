4278a9a2627445fd6e8d661f8cd134417a33bba1
cristian <cristian.ortiz@uda.cl>
2020-05-02 22:35:52 -0400

    dar estilo dinamicamente a componentes mediante objetos de JS

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,16 +2,32 @@
 import React, { Component } from "react";
 
 class Task extends Component {
+  //metodo para dar estilo de forma dinamica segun eventos JS
+  styleCompleted() {
+    return {
+      fontSize: "20px",
+      color: this.props.task.done ? "blue" : "black", //cambio el color segun si task.done = true/false
+    };
+  }
+
   render() {
     //extraigo desde props los datos de una tarea
     const { task } = this.props;
     return (
-      <div>
-        {task.title} - {task.description} -{task.done} - {task.id}
+      <div style={this.styleCompleted()}>
+        {task.title} - {task.description} - {task.done} - {task.id}
         <input type="checkbox" />
-        <button>x</button>
+        <button style={btnDelete}>x</button>
       </div>
     );
   }
 }
 export default Task;
+//estilo css a traves de un objeto JS
+const btnDelete = {
+  fontSize: "15px",
+  background: "#ea2027",
+  border: "none",
+  padding: "10px",
+  borderRadius: "10%",
+};
